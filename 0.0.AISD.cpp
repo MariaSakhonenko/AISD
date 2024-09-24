0fb1edb682328515f3dad9cafda3a69f12ce569a
Maria Sakhonenko <maria.sakhonenko@gmail.com>
2024-09-24 18:52:23 +0300

    0.0

---
+++ 0.0.AISD.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <fstream>
+#include <set>
+using namespace std;
+
+int main()
+{
+	ifstream inFile("input.txt");
+	ofstream outFile("output.txt");
+
+	if (!inFile.is_open() || !outFile.is_open())
+	{
+		cout << "Error opening file" << endl;
+		return 1;
+	}
+
+	set<int> BezPovtorov;
+	int number;
+
+	while (inFile >> number)
+	{
+		BezPovtorov.insert(number);
+	}
+
+	long long sum = 0;
+
+	for (int num : BezPovtorov)
+	{
+		sum += num;
+	}
+
+	outFile << sum << endl;
+
+	inFile.close();
+	outFile.close();
+
+	return 0;
+}
